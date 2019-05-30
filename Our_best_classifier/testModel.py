14e2eb3df0589e23a678b023f25624e0290ab660
kkzheng <43946960+kkzheng@users.noreply.github.com>
2019-05-30 11:00:27 +0800

    Update testModel.py

--- Our_best_classifier/testModel.py
+++ Our_best_classifier/testModel.py
@@ -8,17 +8,7 @@ def load_data(dirname):
     dictMats = {}
     dictLabels = {}
     j = 0
-    # for i in range(0,12):
     for i in [3,4,5,9,10,11]:
-        # matrixPos = sio.loadmat(dirname+'\\'+'pos'+str(i)+'.mat')
-        # matrixPos = matrixPos['feature']
-        # lablePos = np.ones((matrixPos.shape[0],1))
-        # matrixNeg = sio.loadmat(dirname+'\\'+'neg'+str(i)+'.mat')
-        # matrixNeg = matrixNeg['feature']
-        # labelNeg = np.zeros((matrixNeg.shape[0],1))
-        # matrix = np.vstack([matrixPos,matrixNeg])
-        # label = np.vstack([lablePos,labelNeg])
-
         matrix = sio.loadmat(dirname + '\\' + 'sample' + str(i) + '.mat')
         matrix = matrix['feature']
         lablePos = np.ones((173, 1))
@@ -56,19 +46,6 @@ def encoder(dictMats,dictLabels,ccModel,ccIndex):
     # 返回链数\链数乘分类器数
     return pEncoder
 
-def oneEncoder(dictMats,dictLabels,ccModel,ccIndex):
-    for j in range(len(dictMats)):
-        clf = ccModel[j]
-        index = ccIndex[j]
-        if j == 0:
-            labelPre = clf.predict(dictMats[int(index)])
-            labelPres = labelPre[:, np.newaxis]
-        else:
-            labelPre = clf.predict(np.hstack([dictMats[int(index)], labelPres]))
-            labelPres = np.hstack([labelPres, labelPre[:, np.newaxis]])
-    labelPre = labelPre.reshape(-1, 1)
-    return labelPre
-
 def caucalate_metrics(y_true,y_pred):
     TP = np.sum(y_true[y_pred==1]==1)
     TN = np.sum(y_true[y_pred==0]==0)
@@ -98,51 +75,5 @@ def mutilTest():
     print("多条链:")
     print("accuracy_score: ", caucalate_metrics(y,y_pred))
 
-def oneChainTest():
-    filename = r'E:\test1\myCC\onechain\CCmodel.pickle'
-    indexfile = r'E:\test1\myCC\onechain\index.txt'
-    modelfile = r'E:\test1\myCC\onechain\clf.pickle'
-    dictMats, dictLabels = load_data(dirname)
-    ccModel = load_model(filename)
-    ccIndex = load_model(indexfile)
-    clf = load_model(modelfile)
-    X_test = oneEncoder(dictMats, dictLabels, ccModel, ccIndex)
-    y = np.hstack([np.ones((1, 173)), np.zeros((1, 252))])[0]
-    y_pred = clf.predict(X_test)
-    print("一条链:")
-    print("accuracy_score: ", caucalate_metrics(y, y_pred))
-
-def cross_validation():
-    filename = r'E:\test1\myCC\fea_sele_0.005_0.7482352941176471\CCmodel.pickle'
-    indexfile = r'E:\test1\myCC\fea_sele_0.005_0.7482352941176471\index.txt'
-    modelfile = r'E:\test1\myCC\fea_sele_0.005_0.7482352941176471\clf.pickle'
-    dir = r'F:\testtesttest\Anti-inflammatory_repeate\fea_after_select\0.005\test'
-    dictMats, dictLabels = load_data(dir)
-    ccModel = load_model(filename)
-    ccIndex = load_model(indexfile)
-
-    dictTrainM = {}
-    dictTestM = {}
-    dictTrainL = {}
-    dictTestL = {}
-    skf = StratifiedKFold(n_splits=5)
-    kf = KFold(n_splits=10)
-    Index = np.ones((425, 1))
-    y = np.hstack([np.ones((1, 173)), np.zeros((1, 252))])[0]
-    scores = []
-    for train_index, test_index in skf.split(Index,y):
-        for i in range(len(dictMats)):
-            dictTrainM[i] = dictMats[i][train_index]
-            dictTestM[i] = dictMats[i][test_index]
-            dictTrainL[i] = dictLabels[i][train_index]
-            dictTestL[i] = dictLabels[i][test_index]
-        X_test = encoder(dictTestM, dictTestL, ccModel, ccIndex)
-        clf = load_model(modelfile)
-        y_pred = clf.predict(X_test)
-        scores.append(accuracy_score(y_pred, dictTestL[0].reshape(1, -1)[0]))
-    return scores
-
 if __name__ == '__main__':
     mutilTest()
-    # oneChainTest()
-    # cross_validation()
