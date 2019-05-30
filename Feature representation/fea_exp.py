6141dbb4c8f284f9a9933b5a228f9a331600ad56
kkzheng <43946960+kkzheng@users.noreply.github.com>
2019-05-30 10:26:11 +0800

    Add files via upload

---
+++ Feature representation/fea_exp.py
@@ -0,0 +1,13 @@
+import numpy as np
+def fifty_seven(matrix,protein):
+    # alfabeto = ['A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W','Y']
+    alfabeto = ['A','R','N','D','C','Q','E','G','H','I','L','K','M','F','P','S','T','W','Y','V']
+    res = np.zeros((57,len(protein)))
+    le = len(protein)
+    for i in range(len(protein)):
+        index = [j for j,x in enumerate(alfabeto) if x == protein[i]]
+        if index == []:
+            res[:,i] = 0
+        else:
+            res[:,i] = matrix[:,index[0]]
+    return res
